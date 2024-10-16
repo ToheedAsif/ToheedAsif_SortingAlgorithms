2162cdc09a7d43628087344bbf2e157d0227c967
Toheed Asif <toheed.asif@bp.com>
2024-10-16 11:34:19 +0100

    Adding a new sorting algorithm class and setup a merge sort method

---
+++ Sorting_Algorithms.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+public class Sorting_Algorithms
+{
+    
+    public List<int> Merge_Sort(List<int> numbers)
+    {
+        List<int> sorted = new List<int>();
+        return sorted;
+    }
+
+   
+}
